730ba8d9129c6fa742580106a5e45f22332a5d7d
krzpiszcz <kpiszcz@yahoo.com>
2021-08-01 13:08:14 +0200

    OK

---
+++ KeyVaulet_ex1.ps1
@@ -0,0 +1,13 @@
+#variables
+$ResourceGroupName = 'KP-KeyVault_EX1'
+$Location = 'westeurope'
+$SQLServerName = 'aslsoftsqlserver'
+$DBName = 'MyDB'
+$KeyVaultName = 'aslsoftSecureKeyVault'
+$dbconnectionstring = 'dbconnection'
+
+#Connect-AzAccount
+#Get-AzSubscription
+
+#create new resource group
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -Force 
